6c825b8405dac7c47e4c2b33871b50c611e81fee
Taqui <taqui@kth.se>
2021-04-22 23:56:21 +0200

    Added Terraform to the development environment

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "frontend"
+}
+
+variable "project" {
+  default = "exec-tut-cicd-kubernetes-react"
+}
+
+variable "location" {
+  default = "europe-west2"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
