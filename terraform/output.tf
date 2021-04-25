aeee1012534efb240c96910ece4ac1af11650b85
Taqui <taqui@kth.se>
2021-04-25 16:03:14 +0200

    Created inital env for step1

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "kubernetes_endpoint" {
-  sensitive = true
-  value     = module.gke.endpoint
-}
-
-output "http_load_balancing_enabled" {
-    value   = module.gke.http_load_balancing_enabled
-  
-}
