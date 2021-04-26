f7cddda22dd4b42a0e027519401222e95cbad125
Taqui <taqui@kth.se>
2021-04-26 19:04:53 +0200

    Added a small Easter Egg

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 
 const App = () => {
-  const [counter,setCounter] = useState(5);
+  const [counter,setCounter] = useState(0);
   return (
     <div className="App">
       <h1>Counter</h1>
       <h3>{counter}</h3>
       <button onClick={()=> setCounter(counter+1)}>+</button>
       <button>-</button>
+      {counter === 7 ? <h1>There are seven deadly sins and you are the worst one</h1> : ""}
+      
     </div>
   );
 }
